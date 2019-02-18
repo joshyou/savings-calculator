f070ef9283ae7180a04fc88a547868aa3a3ccaa5
Josh You <joshyou12@gmail.com>
2019-02-17 20:23:44 -0600

    added contribution frequency options

--- src/App.js
+++ src/App.js
@@ -35,39 +35,52 @@ user input - target, principal, amount, interest rate.
 prints number of years
 */
 
-class Years extends React.Component {
+/*npm start to run*/
+
+/* customization frequency: dropdown after contribution box:
+per year, per month, per week?
+
+interest rate is ambiguous. assume it's an annualized interest rate compounded based on period length?
+interest should get converted to period interest - e.g. if period is monthly then interest divided by 12
+this should happen before periodsToTarget is called
+*/
+
+class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state= {target: 100000, 
                  amount: 1000, 
                  interest: 5, 
                  principal: 0,
-                 years: 0};
+                 frequency: 1,
+                 periods: 0};
     this.updateTarget = this.updateTarget.bind(this);
     this.updateAmount = this.updateAmount.bind(this);
     this.updateInterest = this.updateInterest.bind(this);
+    this.updateFrequency = this.updateFrequency.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
-  yearsToTarget(target, principal, amount, interest) {
+  periodsToTarget(target, principal, amount, interest, frequency) {
     let total = principal;
-    let years = 0;
+    let periods = 0;
     //amount = parseFloat(amount);
-    let percentInterest = 1 + 0.01*interest;
+    //alert('frequency:' + frequency)
+    let percentInterest = 1 + 0.01*(interest/frequency);
     if (amount < 0) {
-      return "not gonna happen bud";
+      return "not gonna happen bud!";
     }
     while (total < target) {
       total += amount;
       total *= percentInterest;
-      years += 1;
-      if (years > 10000) {
-        return "not gonna happen bud";
+      periods += 1;
+      if (periods > 10000) {
+        return "not gonna happen bud!";
       }
  
     }
     //alert('final years:' + years)
-    return years;
+    return periods/frequency;
   }
   
   updateTarget(event) {
@@ -85,11 +98,22 @@ class Years extends React.Component {
     this.setState({interest: parseFloat(event.target.value)});
     event.preventDefault();
   }
+
+  updateFrequency(event) {
+    this.setState({frequency: parseFloat(event.target.value)});
+    event.preventDefault();
+  }
   
   handleSubmit(event) {
     
     //this.setState({years:0});
-    this.setState({years: this.yearsToTarget(this.state.target, this.state.principal, this.state.amount, this.state.interest)});
+    this.setState({periods: 
+      this.periodsToTarget(
+        this.state.target, 
+        this.state.principal, 
+        this.state.amount, 
+        this.state.interest, 
+        this.state.frequency)});
     //alert('number of years' + this.state.years);
     event.preventDefault();
     //this.setState({})
@@ -107,8 +131,14 @@ class Years extends React.Component {
         </label>
         <p> </p>
         <label>
-          Annual contribution:&nbsp; 
+          Contribution:&nbsp; 
           <input type="number" value={this.state.amount} onChange={this.updateAmount}/>
+          <select value = {this.state.frequency} onChange={this.updateFrequency}>
+            <option value = {1}>annually</option>
+            <option value = {12}>monthly</option>
+            <option value = {26}>bi-weekly</option>
+            <option value = {52}>weekly</option>
+          </select>
         </label>
         <p></p>
           <label>
@@ -119,16 +149,16 @@ class Years extends React.Component {
         <input type="submit" value="Submit"/>
       </form>
       
-      <p>Years left to savings target: {this.state.years}
+      <p>Years left to savings target: {this.state.periods}
       </p>
       </div>);
   }  
 }
 
 ReactDOM.render(
-  <Years />,
+  <Calculator />,
   //formatName(user),
   document.getElementById('root')
 );
 
-export default Years;
+export default Calculator;
