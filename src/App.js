718fc2919edf03ade3218e72a6603bf8dfea0576
Josh You <joshyou12@gmail.com>
2019-02-19 23:00:09 -0600

    added functionality for multiple calculator options, and principal input

--- src/App.js
+++ src/App.js
@@ -10,20 +10,37 @@ per year, per month, per week?
 interest rate is ambiguous. assume it's an annualized interest rate compounded based on period length?
 interest should get converted to period interest - e.g. if period is monthly then interest divided by 12
 this should happen before periodsToTarget is called
+
+compounding options - right now it's compounded based on contribution frequency.
+this is arbitrary and not realistic.
+options - allow user to specify how often to compound
+-always compound once per year, because interest rates are usually expressed
+as annualized
+-compound daily or continuously, but calculate the interest rate to be equivalent 
+to compounding once per year at the inputted interest rate
+this is NOT equivalent to just dividing the interest rate by 365 and compounding daily
+as doing that leads to faster growth than compounding once per year
 */
 
-class Calculator extends React.Component {
+
+class SavingsCalculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {target: 100000, 
                  amount: 1000, 
                  interest: 5, 
-                 principal: 0,
+                 principal: 1000,
                  frequency: 1,
                  periods: 0,
-                 outputFrequency: 1};
+                 outputFrequency: 1,
+                 show: props.show};
     this.updateState = this.updateState.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //update this.state.show when parent state changes
+  componentWillReceiveProps(nextProps) {
+    this.setState({ show: nextProps.show });  
   }
   
   periodsToTarget(target, principal, amount, interest, frequency) {
@@ -44,10 +61,7 @@ class Calculator extends React.Component {
       }
  
     }
-    //alert('final years:' + years)
     return periods
-
-    //return Math.round( (periods/frequency) * 10 ) / 10;
   }
   
   updateState(param, event) {
@@ -65,7 +79,6 @@ class Calculator extends React.Component {
       output += 'Months'
     }
     output += ' left until savings target: '
-    //return Math.round( (periods/frequency) * 10 ) / 10;
     output += Math.round((periods/frequency)*outputFrequency*10) / 10;
     return output;
   }
@@ -84,18 +97,24 @@ class Calculator extends React.Component {
   }
   
   render() {
-  return (<div>
-      <h3>Savings Calculator</h3>
-      
+    if (this.state.show != 1) {
+      return null;
+    }
+    return (<div>
       <form onSubmit = {this.handleSubmit}>
         <label>
           Savings target:&nbsp; 
-          <input type="number" value={this.state.target} onChange={this.updateState.bind(this, "target")}/>
+          <input type="number" value={this.state.target} min = {0} onChange={this.updateState.bind(this, "target")}/>
         </label>
         <p> </p>
         <label>
+          Principal:&nbsp; 
+          <input type="number" value={this.state.principal} onChange={this.updateState.bind(this, "principal")}/>
+          </label>
+        <p></p>
+        <label>
           Contribution:&nbsp; 
-          <input type="number" value={this.state.amount} onChange={this.updateState.bind(this, "amount")}/>
+          <input type="number" value={this.state.amount} min = {this.state.principal > 0 ? 0 : 1} onChange={this.updateState.bind(this, "amount")}/>
           <select value = {this.state.frequency} onChange={this.updateState.bind(this, "frequency")}>
             <option value = {1}>annually</option>
             <option value = {12}>monthly</option>
@@ -106,7 +125,7 @@ class Calculator extends React.Component {
         <p></p>
           <label>
           Interest rate (%):&nbsp; 
-          <input type="number" value={this.state.interest} onChange={this.updateState.bind(this, "interest")}/>
+          <input type="number" value={this.state.interest} min = {0} onChange={this.updateState.bind(this, "interest")}/>
           </label>
         <p></p>
           <label>
@@ -118,12 +137,79 @@ class Calculator extends React.Component {
           </label>
         <p></p>
         <input type="submit" value="Submit"/>
-      </form>
+        </form>
       
       <p>{this.formatOutput(this.state.outputFrequency, this.state.frequency, this.state.periods)}
       </p>
       </div>);
   }  
+}
+
+//design: loan balance
+//APY
+//compound period (default monthly)
+//(minimum) payment
+//output - time until loan paid off. also interest paid?
+class LoanCalculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //balance: 1000
+      show: props.show};
+    this.updateState = this.updateState.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //update this.state.show when parent state changes
+  componentWillReceiveProps(nextProps) {
+    this.setState({ show: nextProps.show });  
+  }
+
+  updateState(param, event) {
+    this.setState({[param]: parseFloat(event.target.value)});
+    event.preventDefault();
+  }
+
+  render() {
+    if (this.state.show != 2) {
+      return null;
+    }
+    return(
+      <div>
+        <p>LOAN CALC</p>
+      </div>
+    );
+  }
+}
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {calculatorType: 1};
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState(param, event) {
+    this.setState({[param]: parseFloat(event.target.value)});
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Savings Calculator</h3>
+        <label>
+          Calculator Type:&nbsp;
+          <select value = {this.state.calculatorType} onChange={this.updateState.bind(this,"calculatorType")}>
+            <option value = {1}>Time until savings target</option>
+            <option value = {2}>Loan payment calculator</option>
+          </select>
+        </label>
+        <p></p>
+        <SavingsCalculator show={this.state.calculatorType}/>
+        <LoanCalculator show={this.state.calculatorType}/>
+        
+      </div>);
+  }
 }
 
 ReactDOM.render(
